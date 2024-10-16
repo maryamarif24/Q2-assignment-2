ebdf45db6b9495296deda71bf35ea3f466f483db
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2024-10-16 15:26:35 +0500

    About directory

--- page.tsx
+++ page.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./about.module.css";
 import link from "next/link";
-import Header from "./Components/Header/header";
-import Herosection from "./Components/HeroSection/hero"
-import CTA from "./Components/CTA/cta"
-import Footer from "./Components/Footer/footer";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
+
+
+function AboutPage() {
+      return (
+        <div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">⬅ Back to Home</a>
+                    </li>
+                </ul>
+            </div>
+            <h1 className={styles.h1}>About Page</h1>
+            <p className={styles.p}>Hey! This is Maryam Arif. Student of Computer Science, And Currently enrolled in GIAIC.</p>
+        </div>
+      )}
     
-    <Header></Header>
-
-    <Herosection></Herosection>
-
-    <CTA></CTA>
-
-    <Footer></Footer>
-
-    </div>
-  );
-}
+    export default AboutPage;
